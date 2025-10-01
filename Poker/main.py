d7c5a834d9c154799f9c914234ac90613854bea3
Antonio Pacheco <antoniopgr06@gmail.com>
2025-10-01 18:42:14 -0300

    .

---
+++ Poker/main.py
@@ -0,0 +1,10 @@
+from PokerEnviroment import PokerEnviroment
+from Player import Player
+
+players = [
+  Player("Antonio", 1000),
+  Player("Ana", 1000),
+  Player("Biel", 1000),
+]
+
+poker_env = PokerEnviroment(players)
