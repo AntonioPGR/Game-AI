dc1cf02cbd7d9202f8f4a8a556962bb733f106d3
AntonioPGR <antoniopgr06@gmail.com>
2026-01-09 19:02:46 -0300

    .

--- ChessGame/Enums/MoveType.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class MoveType(Enum):
-  NORMAL = "normal"
-  CASTLE = "castle"
-  EN_PASSANT = "enpassant"
-  PROMOTION = "promotion"
--- ChessGame/Enums/PieceColor.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class PieceColor(Enum):
-  WHITE = "white"
-  BLACK = "black"
